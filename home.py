247cd7543e673e7bcb410958129d7da3e2b590cf
AndreaLeone06 <leone.andrea@iisgalvanimi.edu.it>
2023-03-15 13:16:07 +0000

    es_3 _fatto_

--- home.py
+++ home.py
@@ -2,20 +2,24 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def site():
+def citta():
     return render_template("citta.html")
 
-@app.route('/mappa', methods=['GET'])
-def mappa():
-    return render_template("mappa.html")
+@app.route('/roma', methods=['GET'])
+def roma():
+    return render_template("roma.html")
 
-@app.route('/mappa/<width>', methods=['GET'])
-def mappaWidth(width):
-    return render_template("mappa.html", dimensione=width)
+@app.route('/milano', methods=['GET'])
+def milano():
+    return render_template("milano.html")
 
-@app.route('/template', methods=['GET'])
-def template():
-    return render_template("sito2/index.html")
+@app.route('/pisa', methods=['GET'])
+def pisa():
+    return render_template("pisa.html")
+
+@app.route('/firenze', methods=['GET'])
+def firenze():
+    return render_template("firenze.html")
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
